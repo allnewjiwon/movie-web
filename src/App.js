d809db6da887509d7ddb2b5755910ec95c055b25
jiwon <jiwon970403@gmail.com>
2024-01-17 16:26:54 +0900

    commit1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+/**
+ *  24.01.16 import 순서  
+ * 1. react 관련 라이브러리
+ * 2. 외부 패키지 및 모듈 << ex) import {useState, useEffect} from 'react'; / import axios from 'axios'
+ * 3. 내부 컴포넌트 및 스타일 ex) import MyComponent from './MyComponent'; / import './styles.css';
+ * **/ 
+import React from 'react';
+import {Routes, Route, Link, Outlet} from 'react-router-dom';
+import axios from 'axios';
+import MyComponenet from './pages/Main';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
+      <MyComponenet />
     </div>
   );
 }
---
+++ src/pages/Intro.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../styles/Intro.scss';
+
+function Intro (){
+  return(
+    <div>
+      
+      <img src="../../assets/static/movie01.jpeg" alt="영화포스터"></img>
+      <img src="../../assets/static/movie02.jpeg" alt="영화포스터"></img>
+      <img src="../../assets/static/movie03.jpeg" alt="영화포스터"></img>
+    </div>
+  )
+}
+
+export default Intro;
