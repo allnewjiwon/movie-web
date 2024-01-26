480444511f5dc19dcf83c4d7ebf463fa4d4d1150
lucy-per <vkehthflsmsd@naver.com>
2024-01-26 15:09:09 +0900

    한 번 더~

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,7 +4,7 @@ const MyComponent = () => {
   return (
     <div>
       <h1>Hello, JSX!</h1>
-      <p>커밋이 될가요~ 과연 될까요 ~</p>
+      <p>커밋이 될가요~ 과연 될까요 ~ 한 번 더 ~</p>
       <span>되세요~ 좀 되보세요~</span>
     </div>
   );
