41654804bb29885fe2ef594453808e10a2e074ed
jiwon <jiwon970403@gmail.com>
2024-01-26 17:01:00 +0900

    intro틀 완성

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -19,42 +19,42 @@ import '../scss/main.scss';
 function Intro() {
   return (
     <div>
-
-      <Swiper
-        modules = {[Navigation, Pagination, EffectFade, Autoplay
-        ]}
-        effect="fade"
-        // autoplay={{delay:2000}}
-        // loop={true}
-        // spaceBetween={50}
-        slidesPerView={1}
-        // navigation
-        pagination={{ clickable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-      >
-        <SwiperSlide>
-          <div className="visual-slide-item">
-            <img src={require("../assets/static/movie01.jpg")} alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="visual-slide-item">
-            <img src={require("../assets/static/movie02.jpeg")} alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="visual-slide-item">
-            <img src={require("../assets/static/movie03.jpeg")} alt="" />
-          </div>
-        </SwiperSlide>
-      </Swiper>
-      <div className="tit-box">
-        <span className="idx-txt">ATTENTION</span>
-        <h3 className="title">Introducing this year's movie</h3>
-        <p className="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium animi doloremque perferendis. Reprehenderit consequuntur maiores ut nostrum velit odio autem quos placeat, itaque quaerat? Ipsam et rem molestiae vel consectetur.</p>
-
-        <button type="button" className="page-link-btn">Move to Main</button>
-      </div>  
+      <main className="container">
+        <Swiper
+          modules = {[Navigation, Pagination, EffectFade, Autoplay
+          ]}
+          effect="fade"
+          autoplay={{delay:2000}}
+          // loop={true}
+          // spaceBetween={50}
+          slidesPerView={1}
+          // navigation
+          pagination={{ clickable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide>
+            <div className="visual-slide-item">
+              <img src={require("../assets/static/movie01.jpg")} alt="" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="visual-slide-item">
+              <img src={require("../assets/static/movie02.jpeg")} alt="" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="visual-slide-item">
+              <img src={require("../assets/static/movie03.jpeg")} alt="" />
+            </div>
+          </SwiperSlide>
+        </Swiper>
+        <div className="tit-box">
+          <span className="idx-txt">ATTENTION</span>
+          <h3 className="main-title">Introducing this year's movie</h3>
+          <p className="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium animi doloremque perferendis. Reprehenderit consequuntur maiores ut nostrum velit odio autem quos placeat, itaque quaerat? Ipsam et rem molestiae vel consectetur.</p>
+          <button type="button" className="main-link-btn">MOVE TO MAIN!</button>
+        </div>
+      </main>
     </div>
   );
 }
