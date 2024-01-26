f2916cf8a3436d0215a2ff30d0b45830ea0d5649
jiwon <jiwon970403@gmail.com>
2024-01-26 13:40:05 +0900

    폴더 구성 완료

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -1,15 +1,62 @@
 import React from "react";
-import '../styles/Intro.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, EffectFade, Autoplay } from "swiper/modules";
 
-function Intro (){
-  return(
+import 'swiper/css/bundle';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
+import 'swiper/css/autoplay';
+
+import '../scss/main.scss';
+
+// const imagePaths = [
+//   require("../assets/movie01.jpeg").default,
+//   require("../assets/movie02.jpeg").default,
+//   require("../assets/movie03.jpeg").default,
+// ];
+
+function Intro() {
+  return (
     <div>
-      
-      <img src="../../assets/static/movie01.jpeg" alt="영화포스터"></img>
-      <img src="../../assets/static/movie02.jpeg" alt="영화포스터"></img>
-      <img src="../../assets/static/movie03.jpeg" alt="영화포스터"></img>
+
+      <Swiper
+        modules = {[Navigation, Pagination, EffectFade, Autoplay
+        ]}
+        effect="fade"
+        // autoplay={{delay:2000}}
+        // loop={true}
+        // spaceBetween={50}
+        slidesPerView={1}
+        // navigation
+        pagination={{ clickable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <div className="visual-slide-item">
+            <img src={require("../assets/static/movie01.jpg")} alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="visual-slide-item">
+            <img src={require("../assets/static/movie02.jpeg")} alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="visual-slide-item">
+            <img src={require("../assets/static/movie03.jpeg")} alt="" />
+          </div>
+        </SwiperSlide>
+      </Swiper>
+      <div className="tit-box">
+        <span className="idx-txt">ATTENTION</span>
+        <h3 className="title">Introducing this year's movie</h3>
+        <p className="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium animi doloremque perferendis. Reprehenderit consequuntur maiores ut nostrum velit odio autem quos placeat, itaque quaerat? Ipsam et rem molestiae vel consectetur.</p>
+
+        <button type="button" className="page-link-btn">Move to Main</button>
+      </div>  
     </div>
-  )
+  );
 }
 
 export default Intro;
