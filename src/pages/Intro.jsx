772106a7185466a1a0a6f03f674614262714e14a
jiwon <jiwon970403@gmail.com>
2024-01-28 18:30:54 +0900

    되라되라되라

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -53,7 +53,7 @@ function Intro() {
           <h3 className="main-title">Introducing this year's movie</h3>
           <p className="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium animi doloremque perferendis. Reprehenderit consequuntur maiores ut nostrum velit odio autem quos placeat, itaque quaerat? Ipsam et rem molestiae vel consectetur.</p>
           <button type="button" className="main-link-btn">
-            MOVE TO MAIN!
+            act TO MAIN!
           </button>
         </div>
       </main>
