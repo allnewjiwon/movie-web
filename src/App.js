76d2457b4a439e97d0ea6d1d58bdcbd7f9368189
lucy-per <vkehthflsmsd@naver.com>
2024-01-29 23:19:15 +0900

    0129 api 리스트 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 /**
- *  24.01.16 import 순서  
+ *  24.01.16 import 순서
  * 1. react 관련 라이브러리
  * 2. 외부 패키지 및 모듈 << ex) import {useState, useEffect} from 'react'; / import axios from 'axios'
  * 3. 내부 컴포넌트 및 스타일 ex) import MyComponent from './MyComponent'; / import './styles.css';
- * **/ 
-import React from 'react';
-import {Routes, Route, Link, Outlet} from 'react-router-dom';
-import axios from 'axios';
-import Main from './pages/Main';
-import Intro from './pages/Intro';
-import './App.css';
+ * **/
+import React from "react";
+import { Routes, Route, Link, Outlet } from "react-router-dom";
+import axios from "axios";
+import Main from "./pages/Main";
+import Intro from "./pages/Intro";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-      <Route path="/" element={<Intro />}></Route>
+        <Route path="/" element={<Intro />}></Route>
         <Route path="/Main" element={<Main />}></Route>
       </Routes>
     </div>
-
   );
 }
 
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,55 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
-const Main = () => {
+const API_KEY = "9cf52cfc64015d8eb0b74ce4e9e0a774";
+
+function Main() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    fetch(
+      `http://kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=${API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setMovies(json.movieListResult.movieList);
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <div>
-      <h1>안녕하세요</h1>
-      <h1>안녕하세요 test</h1>
+      <div id="GNB">
+        <p>Home</p>
+        <form>
+          <input type="text" placeholder="search" />
+          <button>Search</button>
+        </form>
+        <p>Contact</p>
+        <p>Log in</p>
+      </div>
+
+      {loading ? (
+        <h1>loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <li key={movie.movieCd}>
+              {movie.movieNm}{" "}
+              {movie.movieNmEn === "" ? "" : "(" + movie.movieNmEn + ")"}
+              <p>제작 연도 : {movie.prdtYear}</p>
+              <p>장르 : {movie.genreAlt}</p>
+              <p>제작 국가 : {movie.nationAlt}</p>
+            </li>
+          ))}
+        </div>
+      )}
     </div>
   );
-};
+}
 
 export default Main;
