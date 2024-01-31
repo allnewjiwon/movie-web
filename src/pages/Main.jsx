896777563ca1ff51058e1ab10a3d49e72e538a04
lucy-per <vkehthflsmsd@naver.com>
2024-01-31 12:07:23 +0900

    0131 api 이미지 로드 완료

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,19 +1,23 @@
 import React, { useEffect, useState } from "react";
 
 const API_KEY = "9d5002da74fa822995bfbbc6f6cb3955";
+const BASE_URL = "https://api.themoviedb.org/3";
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
 
 function Main() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
 
   const getMovies = async () => {
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
+    fetch(`${BASE_URL}/discover/movie?api_key=${API_KEY}`)
       .then((response) => response.json())
       .then((json) => {
         setMovies(json);
         setLoading(false);
       });
   };
+
+  console.log(movies);
 
   useEffect(() => {
     getMovies();
@@ -34,11 +38,14 @@ function Main() {
       {loading ? (
         <h1>loading...</h1>
       ) : (
-        <div>
+        <div className="movies">
           {movies.results.map((movie) => (
             <li key={movie.id}>
-              {movie.original_title}
-              <img src={movie.poster_path}></img>
+              <img src={`${IMAGE_BASE_URL}${movie.backdrop_path}`} />
+              <p>realse date : {movie.release_date}</p>
+              <p>title : {movie.title}</p>
+              <p>summary : {movie.overview}</p>
+              <p>genre : {movie.genre_ids}</p>
             </li>
           ))}
         </div>
