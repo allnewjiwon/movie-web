1a302ce628d0e9b97c23ac1007283e0f1c5c4615
lucy-per <vkehthflsmsd@naver.com>
2024-02-02 18:00:06 +0900

    커밋 완~

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -22,25 +22,21 @@ function Main() {
 
   return (
     <div>
-<<<<<<< Updated upstream
       <header id="header">
-        <h1 className="logo">JJ&nbsp;PROJECT</h1>
+        <h1 className="logo">JJ PROJECT</h1>
         {/* 24.02.02 comment by jiwon 각 네비게이션들은 추후 Link태그 사용해서 라우팅 할 것 */}
         <nav className="gnb">
           <span className="link-home">Home</span>
           <div className="search-area">
-          <form className="search-form">
-            <input type="text" placeholder="search" />
-            <button className="search-btn" >Search</button>
-          </form>
+            <form className="search-form">
+              <input type="text" placeholder="search" />
+              <button className="search-btn">Search</button>
+            </form>
           </div>
           <span className="link-contact">Contact</span>
           <span className="link-login">Login</span>
         </nav>
       </header>
-=======
-      <GNB />
->>>>>>> Stashed changes
 
       {loading ? (
         <h1>loading...</h1>
