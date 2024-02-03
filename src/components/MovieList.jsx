fd73201825c189772b0c280b672bc2fa38da7ff7
jiwon <jiwon970403@gmail.com>
2024-02-03 16:54:08 +0900

    02.03 모달 해결~

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
 import Modal from "./Modal";
 
-function MovieList({ key, coverImg, title, genre, modalOpen}) {
+function MovieList({ key, coverImg, title, genre, onClick}) {
   const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
   
   return (
     <div>
-      <div className="movie-list"> 
+      <div className="movie-list" onClick={onClick}> 
         {coverImg && (<img src={`${IMAGE_BASE_URL}${coverImg}`} alt={title} />)}
         <h2>{title}</h2>
         <p>{genre}</p>
