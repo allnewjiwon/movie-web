a8d15c76eb07ddc7dea93506616d3b1610d92706
jiwon <jiwon970403@gmail.com>
2024-02-03 17:14:56 +0900

    다시

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,41 @@
 import PropTypes from "prop-types";
-import Modal from "./Modal";
+import { useState } from 'react';
 
-function MovieList({ key, coverImg, title, genre, onClick}) {
+import Modal from "../components/Modal";
+
+function MovieList({ key, coverImg, title, genre, onClick, movie}) {
   const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
+  const [selected, setSelected] = useState(null);
+
+  const modalOpen = (movie) => {
+    const modalData = {
+      ...movie,
+      score : movie.vote_average,
+      overview : movie.overview
+    } 
+    setSelected(modalData);
+  }
+
+  const modalClose = () => {
+    setSelected(null);
+  }
   
   return (
     <div>
-      <div className="movie-list" onClick={onClick}> 
+      <div className="movie-list" onClick={() => modalOpen(movie)}> 
         {coverImg && (<img src={`${IMAGE_BASE_URL}${coverImg}`} alt={title} />)}
         <h2>{title}</h2>
         <p>{genre}</p>
       </div>
+      
+      {selected ? <Modal 
+        image_base_url={IMAGE_BASE_URL}
+        poster_path={selected.poster_path}
+        title={selected.title}
+        score={selected.score}
+        overview={selected.overview}
+        modalClose={modalClose}
+      /> : null}
     </div>
   );
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import MovieList from "../components/MovieList";
-import Modal from "../components/Modal";
+
 
 function Main() {
   const API_KEY = "9d5002da74fa822995bfbbc6f6cb3955";
@@ -20,49 +20,24 @@ function Main() {
   useEffect(() => {
     getMovies();
   }, []);
-  const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
-  const [selected, setSelected] = useState(null);
-
-  const modalOpen = (movie) => {
-    const modalData = {
-      ...movie,
-      score : movie.vote_average,
-      overview : movie.overview
-    } 
-    setSelected(modalData);
-  }
-  
-  const modalClose = () => {
-    setSelected(null);
-  }
 
   return (
     <div>
       <Header />
-
       {loading ? (
         <h1>loading...</h1>
       ) : (
         <div>
           {movies.results.map((movie) => (
             <MovieList
-              onClick={()=>modalOpen(movie)}
-              modalClose={modalClose}
               key={movie.id}
               coverImg={movie.poster_path}
               title={movie.title}
               summary={movie.overview}
               genre={movie.genre_ids}
+              movie={movie}
             />
           ))}
-          {selected ? <Modal 
-            image_base_url={IMAGE_BASE_URL}
-            poster_path={selected.poster_path}
-            title={selected.title}
-            score={selected.score}
-            overview={selected.overview}
-            modalClose={modalClose}
-          /> : null}
         </div>
       )}
     </div>
