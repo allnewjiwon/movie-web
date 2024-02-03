84f4c29ba2911678d2b6a60955f68170b63a4a24
lucy-per <vkehthflsmsd@naver.com>
2024-02-03 16:09:51 +0900

    0203 두번재

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+function Header() {
+  return (
+    <header id="header">
+      <h1 className="logo">JJ PROJECT</h1>
+      {/* 24.02.02 comment by jiwon 각 네비게이션들은 추후 Link태그 사용해서 라우팅 할 것 */}
+      <nav className="gnb">
+        <span className="link-home">Home</span>
+        <div className="search-area">
+          <form className="search-form">
+            <input type="text" placeholder="search" />
+            <button className="search-btn">Search</button>
+          </form>
+        </div>
+        <span className="link-contact">Contact</span>
+        <span className="link-login">Login</span>
+      </nav>
+    </header>
+
+    // <div id="GNB">
+    //   <p>Home</p>
+    //   <form>
+    //     <input type="text" placeholder="search" />
+    //     <button>Search</button>
+    //   </form>
+    //   <p>Contact</p>
+    //   <p>Log in</p>
+    // </div>
+  );
+}
+
+export default Header;
